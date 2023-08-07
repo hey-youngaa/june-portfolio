d44696861977b456615ca19db558468ef36d5bd4
Jessica Choi <jessicahchoi93@gmail.com>
2023-08-06 22:36:18 -0400

    worked on modal img component. need to fix style

--- src/components/imgGallery/ImgGallery.jsx
+++ src/components/imgGallery/ImgGallery.jsx
@@ -1,20 +1,60 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Masonry,{ResponsiveMasonry} from 'react-responsive-masonry'
 import './imggallery.css'
+import {FiChevronLeft,FiChevronRight} from 'react-icons/fi'
+import {IoClose} from 'react-icons/io5'
+
 
 function ImgGallery({gallery}) {
+    const [slideNumber, setSlideNumber] = useState(0)
+    const [openModal, setOpenModal] = useState(false)
+    const length = gallery.length 
+
+    const handleOpenModal = (index) => {
+        setSlideNumber(index)
+        setOpenModal(true)
+    }
+
+    const handleCloseModal = () => {
+        setOpenModal(false)
+    }
+
+    const prevSlide = () => {
+        setSlideNumber( slideNumber === 0 ? length - 1 : slideNumber - 1)
+    }
+    
+    const nextSlide = () => {
+       setSlideNumber( slideNumber === length - 1 ? 0 : slideNumber + 1)
+    }
+
   return (
-    <div className='masonryContainer'>
-        <ResponsiveMasonry columnsCountBreakPoints={{350:1, 750:2, 900:3}} >
-            <Masonry gutter='80px'>
-                {gallery.map((item,index) => {
-                    return(
-                        <img key={index} src={item.image} alt={item.alt} />
-                    )
-                })}
-            </Masonry>
-        </ResponsiveMasonry>
-    </div>
+   <>
+        {openModal &&
+            <div className='modalContainer'>
+                <IoClose className='close' onClick={handleCloseModal} />
+                <FiChevronLeft className='previous' onClick={prevSlide} />
+                <img src={gallery[slideNumber].image} className='modalImg' />
+                <FiChevronRight className='next' onClick={nextSlide} />
+            </div>
+        }
+        <div className='masonryContainer'>
+            <ResponsiveMasonry columnsCountBreakPoints={{350:1, 750:2, 900:3}} >
+                <Masonry gutter='80px'>
+                    {gallery.map((item,index) => {
+                        return(
+                            <img 
+                                key={index}
+                                src={item.image}
+                                alt={item.alt}
+                                className='singleImg'
+                                onClick={() => handleOpenModal(index)}
+                            />
+                        )
+                    })}
+                </Masonry>
+            </ResponsiveMasonry>
+        </div>
+    </>
   )
 }
 
